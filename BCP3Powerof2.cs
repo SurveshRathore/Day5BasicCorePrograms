61f75f96757550f5079e84087e95c6b900b5db8e
SurveshRathore <educatedevilboy@gmail.com>
2022-10-17 18:47:23 +0530

    Power of 2

---
+++ BCP3Powerof2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5BasicCoreProgram
+{
+    internal class BCP3Powerof2
+    {
+        public void generatePowerOf2 (int N)
+        {
+            for(int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("2 * {0} = {1}", i, Math.Pow(2,i));
+            }
+            
+        }
+    }
+}
