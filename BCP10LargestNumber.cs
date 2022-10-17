137b55e0e79cf2198ccd25f9f2ceadc04e7e343c
SurveshRathore <educatedevilboy@gmail.com>
2022-10-17 18:52:41 +0530

    C# Program to Find the Largest Among Three Numbers

---
+++ BCP10LargestNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5BasicCoreProgram
+{
+    internal class BCP10LargestNumber
+    {
+        public void findLargestnum (int num1, int num2, int num3)
+        {
+            if( (num1 > num2) && (num1 > num3) )
+            {
+                Console.WriteLine(num1 + " is the largest number.");
+            }
+            else if ((num2 > num1) && (num2 > num3))
+            {
+                Console.WriteLine(num2 + " is the largest number.");
+            }
+            else
+            {
+                Console.WriteLine(num3 + " is the largest number.");
+            }
+
+        }
+    }
+}
