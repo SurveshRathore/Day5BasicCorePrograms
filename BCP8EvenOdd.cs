4661bc7343af524d82c17af6acda86618fe0bbac
SurveshRathore <educatedevilboy@gmail.com>
2022-10-17 18:50:59 +0530

    C# Program to Check Whether a Number is Even or Odd

---
+++ BCP8EvenOdd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5BasicCoreProgram
+{
+    internal class BCP8EvenOdd
+    {
+        public void findEvenOdd(int num)
+        {
+            if(num > 0)
+            {
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine(num + " is the even number.");
+                }
+                else
+                {
+                    Console.WriteLine(num + " is the odd number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid number");
+            }
+            
+        }
+    }
+}
