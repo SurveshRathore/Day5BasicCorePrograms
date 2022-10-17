6a213541f16d41ad211f2ef8276e3ff6b193cc74
SurveshRathore <educatedevilboy@gmail.com>
2022-10-17 18:52:02 +0530

    C# Program to Check Whether an Alphabet is Vowel or Consonant

---
+++ BCP9VowelConsonant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5BasicCoreProgram
+{
+    internal class BCP9VowelConsonant
+    {
+        public void checkAlpVoCons(char alpha)
+        {
+            //convert the character to uppercase
+            alpha = char.ToUpper(alpha);
+
+            if (alpha == 'A' || alpha == 'E' || alpha == 'I' || alpha == 'O' || alpha == 'U')
+            {
+                Console.WriteLine("Alphabet {0} is a vowel.", alpha);
+            }
+            else
+            {
+                Console.WriteLine("Alphabet {0} is a Consonant.", alpha);
+            }
+        }
+    }
+}
